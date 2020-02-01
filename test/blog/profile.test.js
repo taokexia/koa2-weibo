4c01afa8a2c7963710fa2fcd5159a4b83f2cd3de
taokexia <694198149@qq.com>
2020-02-01 18:09:44 +0800

    test: 个人主页 单元测试

---
+++ test/blog/profile.test.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: taokexia
+ * @Date: 2020-02-01 18:01:52
+ * @LastEditTime : 2020-02-01 18:06:54
+ * @LastEditors  : Please set LastEditors
+ * @Description: 个人主页 test
+ * @FilePath: \koa2-weibo-code\test\blog\profile.test.js
+ */
+
+const server  = require('../server')
+const { COOKIE, USER_NAME } = require('../testUserInfo')
+
+test('个人主页，加载第一页数据，应该成功', async () => {
+    const res = await server
+        .get(`/api/profile/loadMore/${USER_NAME}/0`)
+        .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+
+    const data = res.body.data
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+    expect(data).toHaveProperty('count')
+})
