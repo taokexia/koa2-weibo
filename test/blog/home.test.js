b405b3de0ba135dfc8a9c59c12ea97db3bdb6dc0
taokexia <694198149@qq.com>
2020-02-03 23:43:35 +0800

    test: 首页 单元测试

--- test/blog/home.test.js
+++ test/blog/home.test.js
@@ -1,7 +1,7 @@
 /*
  * @Author: taokexia
  * @Date: 2020-02-01 00:23:53
- * @LastEditTime : 2020-02-02 21:16:36
+ * @LastEditTime : 2020-02-03 23:42:19
  * @LastEditors  : Please set LastEditors
  * @Description: 首页test
  * @FilePath: \koa2-weibo-code\test\blog\home.test.js
@@ -34,4 +34,18 @@ test('创建一条微博，应该成功', async () => {
 
     // 记录微博 id
     BLOG_ID = res.body.data.id
+})
+
+// 加载第一页数据
+test('首页，加载第一页数据', async () => {
+    const res = await server
+        .get(`/api/blog/loadMore/0`)
+        .set('cookie', T_COOKIE)
+    expect(res.body.errno).toBe(0)
+    const data = res.body.data
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+    expect(data).toHaveProperty('count')
 })
--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -1,7 +1,7 @@
 /*
  * @Author: taokexia
  * @Date: 2020-02-01 00:29:32
- * @LastEditTime : 2020-02-02 21:19:51
+ * @LastEditTime : 2020-02-03 23:41:13
  * @LastEditors  : Please set LastEditors
  * @Description: 单元测试的用户信息
  * @FilePath: \koa2-weibo-code\test\testUserInfo.js
@@ -14,10 +14,10 @@
 module.exports = {
     T_ID: 1,
     T_USER_NAME: 'taokexia',
-    T_COOKIE: 'weibo.sid=CGZA7KJgZmyevaRhhklQjz6hhpeRa4kq; weibo.sid.sig=lPqnxu8DO-9vYseG8eAYrFQRO2o',
+    T_COOKIE: 'weibo.sid=7q_KPn_gMUAWTwVVZSE6qhe9MILux7Ng; weibo.sid.sig=9gWKceSGNFJDF33XM56mptfy3ZU',
 
     Z_ID: 2,
-    Z_USER_NAME: 'zhangsan',
+    Z_USER_NAME: 'lisi',
     Z_COOKIE: 'weibo.sid=CBHe2p58UctIEAJgicsSdczi8zr-9VbT; weibo.sid.sig=5dIU3AYzr6OzLRUhSwd7964NA3U'
 
 }
